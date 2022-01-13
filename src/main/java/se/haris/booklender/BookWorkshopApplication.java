ea73a346622076dbcef871669d5c66a6ad33130b
hgusi <hgusinac@gmail.com>
2022-01-13 08:57:57 +0100

    Initial commit

---
+++ src/main/java/se/haris/booklender/BookWorkshopApplication.java
@@ -0,0 +1,13 @@
+package se.haris.booklender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/haris/booklender/BookWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.haris.booklender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
